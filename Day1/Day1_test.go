a08f25605435812d7eb473faa8a429b739f76db2
Nate Pink <nate.pink@craftjack.com>
2019-12-03 07:34:51 -0600

    day1/1 update

--- Day1/Day1_test.go
+++ Day1/Day1_test.go
@@ -10,13 +10,6 @@ import (
 	"testing"
 )
 
-func TestSum(t *testing.T) {
-	total := Sum(5, 5)
-	if total != 10 {
-		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
-	}
-}
-
 func Test2(t *testing.T) {
 	mass := 12
 	fuel := CalcFuel(mass)
